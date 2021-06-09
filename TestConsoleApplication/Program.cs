d191f2ba2d98f21f434e66ede00f1444e4d5ad8d
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2021-06-09 08:33:23 -0300

    Added test console application;

---
+++ TestConsoleApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿#if !UNITY_2017_1_OR_NEWER
+using System;
+using System.Collections.Generic;
+using System.IO;
+using BinarySerializationTests.Models;
+using BinarySerializer;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            List<GraphClass> classes = new List<GraphClass>();
+            for (int index = 0; index < 10000; index++)
+                classes.Add(new GraphClass(new[] {"str1"},
+                    new[] {20010L},
+                    "str2", 103,
+                    new byte[] {193, 59},
+                    23958533F));
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Serializer.Serialize(classes, ms);
+                
+                ms.Position = 0;
+
+                List<GraphClass> newClasses = (List<GraphClass>) Deserializer.Deserialize(ms);
+                
+                Console.WriteLine(newClasses.Count);
+            }
+        }
+    }
+}
+#endif
